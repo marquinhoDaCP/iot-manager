daa97b9d90e4756639e37591aff4c77a5281fa86
Marcus Vinicius <marcus.eevil1@gmail.com>
2025-12-03 01:05:42 -0400

    initial commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DevicesComponent } from './pages/devices/devices.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DevicesComponent,
+    LoginComponent,
+    SignupComponent,
+    ReportsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
