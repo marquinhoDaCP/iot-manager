1f3f0e9236107811c5dab99a8d8737ec0a0b1e61
Marcus Vinicius Azevedo <marcus.azevedo@iatecam.org.br>
2025-12-03 21:26:47 -0400

    feat: added animation in the side menu

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { DevicesComponent } from './pages/devices/devices.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
 const routes: Routes = [
   {
@@ -12,6 +14,18 @@ const routes: Routes = [
     path: 'signup',
     component: SignupComponent
   },
+  {
+    path: 'devices',
+    component: DevicesComponent
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent
+  },
+  {
+    path: 'dashboard',
+    component: DevicesComponent
+  }
 ];
 
 @NgModule({
--- frontend/src/app/components/lateral-menu/lateral-menu.component.ts
+++ frontend/src/app/components/lateral-menu/lateral-menu.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-lateral-menu',
   templateUrl: './lateral-menu.component.html',
   styleUrls: ['./lateral-menu.component.scss']
 })
-export class LateralMenuComponent {
+export class LateralMenuComponent implements OnInit {
+  @ViewChildren('menuItems') menuItems!: QueryList<ElementRef<HTMLLIElement>>;
+  @ViewChild('activeTab') activeTab!: ElementRef<HTMLDivElement>;
 
+  constructor(
+    private _router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    this._router.events.subscribe((event) => this.routeChangeEvent(event));
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    const path = event instanceof NavigationEnd ? event.url.replace("/", "") : this._router.url.replace("/", "");
+    const activeMenuItem = this.menuItems.toArray().find(item => item.nativeElement.id === `router_${path}`);
+    if (!activeMenuItem) return;
+    const { offsetTop, offsetHeight, offsetWidth } = activeMenuItem.nativeElement;
+
+    const newHeight = offsetHeight * 2;
+    const newWidth = offsetWidth * 0.6;
+    const top = offsetTop - (newHeight - offsetHeight) / 2;
+
+    this.activeTab.nativeElement.style.height = `${newHeight}px`;
+    this.activeTab.nativeElement.style.width = `${newWidth}px`;
+    this.activeTab.nativeElement.style.top = `${top}px`;
+  }
+
+  routeChangeEvent(event: any) {
+    if (event instanceof NavigationEnd) {
+      this.onResize(event);
+    }
+  }
+
+  navigateTo(path: string) {
+    this._router.navigate([path]);
+  }
 }
